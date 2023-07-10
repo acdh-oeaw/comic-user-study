d6ce31b935ba22bac6fd3823ec36ab4437ae0f5d
Katharina Wünsche <katharina_wunsche@yahoo.de>
2023-07-10 15:02:47 +0200

    feat: randomize multiple choice options

--- src/components/quiz/MultipleChoice.tsx
+++ src/components/quiz/MultipleChoice.tsx
@@ -1,4 +1,4 @@
-import { type ReactElement, type ReactNode } from "react";
+import { type ReactElement, type ReactNode, useEffect } from "react";
 import { useState } from "react";
 
 import { getChildElements } from "@/components/quiz/getChildElements";
@@ -12,6 +12,32 @@ export interface MultipleChoiceProps {
 	name: string;
 }
 
+// From https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+function shuffle(array: Array<ReactNode>) {
+	let currentIndex = array.length,
+		randomIndex;
+
+	// While there remain elements to shuffle.
+	while (currentIndex !== 0) {
+		// Pick a remaining element.
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex--;
+
+		// And swap it with the current element.
+		[array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+	}
+
+	return array;
+}
+
+export async function getStaticProps(o: Array<ReactNode>) {
+	return {
+		props: {
+			shuffledOptions: shuffle(o),
+		},
+	};
+}
+
 /**
  * Multiple choice quiz.
  */
@@ -20,12 +46,19 @@ export function MultipleChoice(props: MultipleChoiceProps): JSX.Element {
 
 	const childElements = getChildElements(props.children);
 	const options = childElements.filter(isMultipleChoiceOption);
-	const correctAnswers = options
+
+	const [shuffledOptions, setShuffledOptions] = useState([]);
+	useEffect(() => {
+		const randomOptions = shuffle(options);
+		setShuffledOptions(randomOptions);
+	}, []);
+
+	const correctAnswers = shuffledOptions
 		.filter((option) => {
 			return option.props.isCorrect === true;
 		})
 		.map((option) => {
-			return options.indexOf(option);
+			return shuffledOptions.indexOf(option);
 		});
 	const isSingleChoice = props.variant === "single";
 	/**
@@ -50,8 +83,6 @@ export function MultipleChoice(props: MultipleChoiceProps): JSX.Element {
 	}
 
 	function optionToString(option: any) {
-		console.log(option.props?.children);
-		console.log(option.props?.children?.props?.children);
 		return option.props?.children?.props?.children;
 	}
 
@@ -64,7 +95,7 @@ export function MultipleChoice(props: MultipleChoiceProps): JSX.Element {
 		trackQuizAnswer(
 			"Navigate to next quiz answer",
 			props.name,
-			[...checked].map((i) => optionToString(options[i])).join(", "),
+			[...checked].map((i) => optionToString(shuffledOptions[i])).join(", "),
 			isCorrect,
 		);
 		quiz.next();
@@ -78,7 +109,7 @@ export function MultipleChoice(props: MultipleChoiceProps): JSX.Element {
 		trackQuizAnswer(
 			"Navigate to previous quiz answer",
 			props.name,
-			[...checked].map((i) => optionToString(options[i])).join(", "),
+			[...checked].map((i) => optionToString(shuffledOptions[i])).join(", "),
 			isCorrect,
 		);
 		quiz.previous();
@@ -93,7 +124,7 @@ export function MultipleChoice(props: MultipleChoiceProps): JSX.Element {
 		trackQuizAnswer(
 			"Validate quiz answer",
 			props.name,
-			[...checked].map((i) => optionToString(options[i])).join(", "),
+			[...checked].map((i) => optionToString(shuffledOptions[i])).join(", "),
 			isCorrect,
 		);
 		quiz.setStatus(isCorrect === true ? QuizCardStatus.CORRECT : QuizCardStatus.INCORRECT);
@@ -104,7 +135,7 @@ export function MultipleChoice(props: MultipleChoiceProps): JSX.Element {
 
 	const component = (
 		<ul className="quiz-multiple-choice flex flex-col space-y-4">
-			{options.map((option, index) => {
+			{shuffledOptions.map((option, index) => {
 				return (
 					<li key={index}>
 						<label className="flex items-center space-x-4">
